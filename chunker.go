080a9912dcbd8d5d455fb3d478026a56b5fdc00b
Valentin Vidic <vvidic@valentin-vidic.from.hr>
2020-11-29 12:38:43 +0100

    Split chunker and pubsub into separate files

---
+++ chunker.go
@@ -0,0 +1,214 @@
+/*
+ * mjpeg-proxy -- Republish a MJPEG HTTP image stream using a server in Go
+ *
+ * Copyright (C) 2015-2020, Valentin Vidic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+/* Sample source stream starts like this:
+
+   HTTP/1.1 200 OK
+   Content-Type: multipart/x-mixed-replace;boundary=myboundary
+   Cache-Control: no-cache
+   Pragma: no-cache
+
+   --myboundary
+   Content-Type: image/jpeg
+   Content-Length: 36291
+
+   JPEG data...
+*/
+
+type Chunker struct {
+	id       string
+	source   string
+	username string
+	password string
+	resp     *http.Response
+	boundary string
+	stop     chan struct{}
+}
+
+func NewChunker(id, source, username, password string) (*Chunker, error) {
+	chunker := new(Chunker)
+
+	sourceUrl, err := url.Parse(source)
+	if err != nil {
+		return nil, err
+	}
+	if !sourceUrl.IsAbs() {
+		return nil, fmt.Errorf("uri is not absolute: %s", source)
+	}
+
+	chunker.id = id
+	chunker.source = source
+	chunker.username = username
+	chunker.password = password
+
+	return chunker, nil
+}
+
+func (chunker *Chunker) Connect() error {
+	fmt.Printf("chunker[%s]: connecting to %s\n", chunker.id, chunker.source)
+
+	req, err := http.NewRequest("GET", chunker.source, nil)
+	if err != nil {
+		return err
+	}
+
+	if chunker.username != "" && chunker.password != "" {
+		req.SetBasicAuth(chunker.username, chunker.password)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		chunker.closeResponse(resp)
+		return fmt.Errorf("request failed: %s", resp.Status)
+	}
+
+	boundary, err := getBoundary(resp)
+	if err != nil {
+		chunker.closeResponse(resp)
+		return err
+	}
+
+	chunker.resp = resp
+	chunker.boundary = boundary
+	chunker.stop = make(chan struct{})
+	return nil
+}
+
+func (chunker *Chunker) closeResponse(resp *http.Response) {
+	err := resp.Body.Close()
+	if err != nil {
+		fmt.Printf("chunker[%s]: body close failed: %s\n", chunker.id, err)
+	}
+}
+
+func getBoundary(resp *http.Response) (string, error) {
+	contentType := resp.Header.Get("Content-Type")
+	mediaType, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return "", err
+	}
+	if !strings.HasPrefix(mediaType, "multipart/") {
+		return "", fmt.Errorf("expected multipart media type: %s", contentType)
+	}
+
+	boundary := params["boundary"]
+	if boundary == "" {
+		return "", fmt.Errorf("boundary not found: %s", contentType)
+	}
+
+	return boundary, nil
+}
+
+func (chunker *Chunker) GetHeader() http.Header {
+	return chunker.resp.Header
+}
+
+func (chunker *Chunker) Start(pubChan chan []byte) {
+	fmt.Printf("chunker[%s]: started\n", chunker.id)
+
+	body := chunker.resp.Body
+	defer func() {
+		err := body.Close()
+		if err != nil {
+			fmt.Printf("chunker[%s]: body close failed: %s\n", chunker.id, err)
+		}
+	}()
+	defer close(pubChan)
+
+	var failure error
+	mr := multipart.NewReader(body, chunker.boundary)
+
+ChunkLoop:
+	for {
+		part, err := mr.NextPart()
+		if err == io.EOF {
+			break ChunkLoop
+		}
+		if err != nil {
+			failure = err
+			break ChunkLoop
+		}
+
+		data, err := ioutil.ReadAll(part)
+		if err != nil {
+			failure = err
+			break ChunkLoop
+		}
+
+		err = part.Close()
+		if err != nil {
+			failure = err
+			break ChunkLoop
+		}
+
+		if len(data) == 0 {
+			failure = errors.New("received final chunk of size 0")
+			break ChunkLoop
+		}
+
+		select {
+		case <-chunker.stop:
+			break ChunkLoop
+		case pubChan <- data:
+		}
+	}
+
+	if failure != nil {
+		fmt.Printf("chunker[%s]: failed: %s\n", chunker.id, failure)
+	} else {
+		fmt.Printf("chunker[%s]: stopped\n", chunker.id)
+	}
+}
+
+func (chunker *Chunker) Stop() {
+	fmt.Printf("chunker[%s]: stopping\n", chunker.id)
+	close(chunker.stop)
+}
+
+func (chunker *Chunker) Started() bool {
+	if chunker.stop == nil { // Never started
+		return false
+	}
+
+	select {
+	case <-chunker.stop: // Already stopped
+		return false
+	default:
+		return true // Still running
+	}
+}
---
+++ pubsub.go
@@ -0,0 +1,250 @@
+/*
+ * mjpeg-proxy -- Republish a MJPEG HTTP image stream using a server in Go
+ *
+ * Copyright (C) 2015-2020, Valentin Vidic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"strings"
+	"time"
+)
+
+type Subscriber struct {
+	RemoteAddr   string
+	ChunkChannel chan []byte
+}
+
+type PubSub struct {
+	id          string
+	chunker     *Chunker
+	pubChan     chan []byte
+	subChan     chan *Subscriber
+	unsubChan   chan *Subscriber
+	subscribers map[*Subscriber]struct{}
+	stopTimer   *time.Timer
+}
+
+func NewSubscriber(client string) *Subscriber {
+	sub := new(Subscriber)
+
+	sub.RemoteAddr = client
+	sub.ChunkChannel = make(chan []byte)
+
+	return sub
+}
+
+func NewPubSub(id string, chunker *Chunker) *PubSub {
+	pubSub := new(PubSub)
+
+	pubSub.id = id
+	pubSub.chunker = chunker
+	pubSub.subChan = make(chan *Subscriber)
+	pubSub.unsubChan = make(chan *Subscriber)
+	pubSub.subscribers = make(map[*Subscriber]struct{})
+	pubSub.stopTimer = time.NewTimer(0)
+	<-pubSub.stopTimer.C
+
+	return pubSub
+}
+
+func (pubSub *PubSub) Start() {
+	go pubSub.loop()
+}
+
+func (pubSub *PubSub) Subscribe(s *Subscriber) {
+	pubSub.subChan <- s
+}
+
+func (pubSub *PubSub) Unsubscribe(s *Subscriber) {
+	pubSub.unsubChan <- s
+}
+
+func (pubSub *PubSub) loop() {
+	for {
+		select {
+		case data, ok := <-pubSub.pubChan:
+			if ok {
+				pubSub.doPublish(data)
+			} else {
+				pubSub.stopChunker()
+				pubSub.stopSubscribers()
+			}
+
+		case sub := <-pubSub.subChan:
+			pubSub.doSubscribe(sub)
+
+		case sub := <-pubSub.unsubChan:
+			pubSub.doUnsubscribe(sub)
+
+		case <-pubSub.stopTimer.C:
+			if len(pubSub.subscribers) == 0 {
+				pubSub.stopChunker()
+			}
+		}
+	}
+}
+
+func (pubSub *PubSub) doPublish(data []byte) {
+	subs := pubSub.subscribers
+
+	for s := range subs {
+		select {
+		case s.ChunkChannel <- data: // try to send
+		default: // or skip this frame
+		}
+	}
+}
+
+func (pubSub *PubSub) doSubscribe(s *Subscriber) {
+	pubSub.subscribers[s] = struct{}{}
+
+	fmt.Printf("pubsub[%s]: added subscriber %s (total=%d)\n",
+		pubSub.id, s.RemoteAddr, len(pubSub.subscribers))
+
+	if len(pubSub.subscribers) == 1 {
+		if err := pubSub.startChunker(); err != nil {
+			fmt.Printf("pubsub[%s]: failed to start chunker: %s\n",
+				pubSub.id, err)
+			pubSub.stopSubscribers()
+		}
+	}
+}
+
+func (pubSub *PubSub) stopSubscribers() {
+	for s := range pubSub.subscribers {
+		close(s.ChunkChannel)
+	}
+}
+
+func (pubSub *PubSub) doUnsubscribe(s *Subscriber) {
+	delete(pubSub.subscribers, s)
+
+	fmt.Printf("pubsub[%s]: removed subscriber %s (total=%d)\n",
+		pubSub.id, s.RemoteAddr, len(pubSub.subscribers))
+
+	if len(pubSub.subscribers) == 0 {
+		if !pubSub.stopTimer.Stop() {
+			select {
+			case <-pubSub.stopTimer.C:
+			default:
+			}
+		}
+		pubSub.stopTimer.Reset(stopDelay)
+	}
+}
+
+func (pubSub *PubSub) startChunker() error {
+	if pubSub.chunker.Started() {
+		return nil
+	}
+
+	err := pubSub.chunker.Connect()
+	if err != nil {
+		return err
+	}
+
+	pubSub.pubChan = make(chan []byte)
+	go pubSub.chunker.Start(pubSub.pubChan)
+
+	return nil
+}
+
+func (pubSub *PubSub) stopChunker() {
+	if pubSub.pubChan != nil {
+		pubSub.chunker.Stop()
+	}
+
+	pubSub.pubChan = nil
+}
+
+func clientAddress(r *http.Request) string {
+	client := r.RemoteAddr
+
+	if clientHeader != "" {
+		header := r.Header.Get(clientHeader)
+		hosts := strings.Split(header, ",")
+		if hosts[0] != "" {
+			client = hosts[0]
+		}
+	}
+
+	return client
+}
+
+func (pubSub *PubSub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// prepare response for flushing
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		fmt.Printf("server[%s]: client %s could not be flushed\n",
+			pubSub.id, r.RemoteAddr)
+		return
+	}
+
+	// subscribe to new chunks
+	sub := NewSubscriber(clientAddress(r))
+	pubSub.Subscribe(sub)
+	defer pubSub.Unsubscribe(sub)
+
+	mw := multipart.NewWriter(w)
+	contentType := fmt.Sprintf("multipart/x-mixed-replace; boundary=%s", mw.Boundary())
+
+	mimeHeader := make(textproto.MIMEHeader)
+	mimeHeader.Set("Content-Type", "image/jpeg")
+
+	headersSent := false
+	for {
+		// wait for next chunk
+		data, ok := <-sub.ChunkChannel
+		if !ok {
+			return
+		}
+
+		// send HTTP header before first chunk
+		if !headersSent {
+			header := w.Header()
+			header.Add("Content-Type", contentType)
+			w.WriteHeader(http.StatusOK)
+			headersSent = true
+		}
+
+		mimeHeader.Set("Content-Size", fmt.Sprintf("%d", len(data)))
+		part, err := mw.CreatePart(mimeHeader)
+		if err != nil {
+			fmt.Printf("server[%s]: part create failed: %s\n", pubSub.id, err)
+			return
+		}
+
+		// send image to client
+		_, err = part.Write(data)
+		if err != nil {
+			fmt.Printf("server[%s]: part write failed: %s\n", pubSub.id, err)
+			return
+		}
+
+		flusher.Flush()
+	}
+
+	err := mw.Close()
+	if err != nil {
+		fmt.Printf("server[%s]: mime close failed: %s\n", pubSub.id, err)
+	}
+}
